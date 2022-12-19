ac8b36f63f267886102fc778808a2b9b3765f225
alanmaranto <alanarostegui@hotmail.com>
2022-12-19 01:59:51 -0600

    feat: add national registry db and setup apis

---
+++ src/api/axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const salesDB = axios.create({
+  baseURL: "http://localhost:3000",
+});
+
+export const nationalRegistryDB  = axios.create({
+  baseURL: "http://localhost:3001",
+});
+
+export const nationalArchivesDB  = axios.create({
+    baseURL: "http://localhost:3002",
+  });
+  
